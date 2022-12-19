9397c579a5bbc97be50f86324889ee6223074dc8
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-12-19 08:51:09 -0800

    pages and components

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import About from './About';
+import Footer from './Footer';
+import Header from './Header';
+import Services from './Services';
+import Testimonials from './Testimonials'
+
+export {
+    About,
+    Footer,
+    Header,
+    Services,
+    Testimonials
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import Listings from './Listings';
+
+export {
+    Home,
+    Listings
+}
